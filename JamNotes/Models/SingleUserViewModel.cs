8b622c756da54cec92cc5fbca1f69bce726df008
Dennis Linsley <denlinsley@gmail.com>
2015-02-10 09:02:58 -0800

    Created new view model

---
+++ JamNotes/Models/SingleUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JamNotes.Models
+{
+    public class SingleUserViewModel
+    {
+        public string UserName { get; set; }
+        public List<Note> Notes { get; set; }
+    }
+}
