0c380c80769cfffd59fd3b8e640fd7c4000404b4
Dennis Linsley <denlinsley@gmail.com>
2015-02-17 13:18:44 -0800

    Noew ViewModel for use in Note/Create

--- JamNotes/Models/SingleUserViewModel.cs
+++ JamNotes/Models/SingleUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,20 @@ namespace JamNotes.Models
     public class SingleUserViewModel
     {
         public string UserName { get; set; }
+        public string Band { get; set; }
+        public string Song { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ShowDate { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Url)]
+        public string Link { get; set; }
+
+
         public List<Note> Notes { get; set; }
     }
 }
